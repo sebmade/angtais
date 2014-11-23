aa6ca300a3cfac8860fe59bb2640e46e22ccb085
Sebastien Letelie <sebastien.letelie@gmail.com>
2014-11-23 17:32:05 +0100

    2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
-function MainCtrl() {
+MainCtrl.$inject = ['$scope', '$document'];
+
+function MainCtrl($scope, $document) {
   var vm = this;
   vm.slideIndex = 0;
   vm.nextSlide = function () {
@@ -12,6 +14,18 @@ function MainCtrl() {
   vm.currentSlide = function () {
     return "slide"+vm.slideIndex+".html";
   };
+
+  $document.keydown(function(event) {
+    if (event.keyCode == 37) {
+      $scope.$apply(function() {
+        vm.previousSlide();
+      });
+    } else if (event.keyCode == 39) {
+      $scope.$apply(function () {
+        vm.nextSlide();
+      });
+    }
+  });
 }
 
 angular.module('angtais').controller('MainCtrl', MainCtrl);
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,11 +5,11 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('angtais'));
 
-  var MainCtrl;
+  var MainCtrl, scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
-    MainCtrl = $controller('MainCtrl');
+    MainCtrl = $controller('MainCtrl', {$scope: scope});
   }));
 
   it('should increment to next slide', function () {
@@ -17,7 +17,7 @@ describe('Controller: MainCtrl', function () {
     MainCtrl.nextSlide();
     expect(MainCtrl.slideIndex).toBe(1);
   });
-  
+
   it('should decrement to next slide', function () {
     MainCtrl.slideIndex = 1;
     MainCtrl.previousSlide();
